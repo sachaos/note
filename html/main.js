df98be1a731a18940d3108c65312b8d447f52f7d
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-08-03 18:48:34 +0900

    Add css & javascript file

---
+++ html/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    if (!window["WebSocket"]) {
+        alert("エラー : WebSocketに対応していないブラウザです。");
+    } else {
+        socket = new WebSocket("ws://localhost:1129/ws");
+        socket.onclose = function() {
+            alert(" 接続が終了しました。");
+        };
+        socket.onmessage = function(e) {
+            var content = document.getElementById('content');
+            content.innerHTML = e.data;
+        };
+    }
+};
